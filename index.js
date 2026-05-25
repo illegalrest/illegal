b0d71f4470d86a8531c5674b004feb52efcc0405
illegal <disk69166@gmail.com>
2026-05-25 10:04:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -66,10 +66,10 @@ app.get("/callback", async (req, res) => {
       ).catch(() => {});
     }
 
-    res.redirect("https://illegal-web.pages.dev/verify.html?success=1");
+    res.redirect("https://illegal-bot.pages.dev/verify.html?success=1");
   } catch (e) {
     console.error(e.response?.data || e.message);
-    res.redirect("https://illegal-web.pages.dev/verify.html?error=1");
+    res.redirect("https://illegal-bot.pages.dev/verify.html?error=1");
   }
 });
 
